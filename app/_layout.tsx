3f1d529b6e7b8688e982d56be3d1d20113ee24f6
Tosin Babatunde <69946111+TosinBabatunde@users.noreply.github.com>
2026-02-06 16:32:15 -0700

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Platform } from 'react-native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
